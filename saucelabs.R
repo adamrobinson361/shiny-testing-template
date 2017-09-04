24e936fb203f03bafd21ab34f77f3c4a88c8abf8
Adam <adam.robinson@education.gov.uk>
2017-09-04 13:16:04 +0100

    Testing sauce labs

---
+++ saucelabs.R
@@ -0,0 +1,38 @@
+library(RSelenium)
+
+user <- "adam.robinson@education.gov.uk" # Your Sauce Labs username
+pass <- "b73a73fa-980c-4c4a-8497-5f5bd232d0da" # Your Sauce Labs access key 
+port <- 80
+ip <- paste0(user, ':', pass, "@ondemand.saucelabs.com")
+rdBrowser <- "chrome"
+version <- "33"
+platform <- "OS X 10.9"
+extraCapabilities <- list(name = "RSelenium OS/Browsers vignette first example", username = user
+                          , accessKey = pass, tags = list("RSelenium-vignette", "OS/Browsers-vignette"))
+
+remDr <- remoteDriver$new(remoteServerAddr = ip, port = port, browserName = rdBrowser
+                          , version = version, platform = platform
+                          , extraCapabilities = extraCapabilities)
+
+testScript <- function(remDr){
+  remDr$open()
+  remDr$navigate("http://www.google.com/ncr")
+  Sys.sleep(2)
+  # highlight the query box
+  remDr$findElement("name", "q")$highlightElement()
+  Sys.sleep(2)
+  # goto rproject
+  remDr$navigate("http://www.r-project.org")
+  # go Back
+  remDr$goBack()
+  # go Forward
+  remDr$goForward()
+  Sys.sleep(2)
+  webElems <- remDr$findElements("css selector", "frame")
+  # highlight the frames
+  lapply(webElems, function(x){x$highlightElement()})
+  
+  remDr$close()
+}
+
+testScript(remDr)
